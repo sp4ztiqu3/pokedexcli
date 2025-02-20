9593b7706639828db78ffa7142f022351f68ff59
Matt Hiddle <matt.hiddle@gmail.com>
2025-02-20 20:31:04 +0800

    add start of simple repl

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	//"fmt"
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		fmt.Printf("Your command was: %v\n", cleanInput(scanner.Text())[0])
+	}
 }
 
 func cleanInput(text string) []string {
