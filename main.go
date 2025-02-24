ca9bc403119506e8a7978cdb53c90d01152c4723
Matt Hiddle <matt.hiddle@gmail.com>
2025-02-24 21:37:04 +0800

    add commands to exit and show help

--- main.go
+++ main.go
@@ -7,13 +7,54 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	commands := map[string]cliCommand{}
+
+	commandExit := func() error {
+		fmt.Println("Closing the Pokedex... Goodbye!")
+		os.Exit(0)
+		return nil
+	}
+	commandHelp := func() error {
+		fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+		for _, c := range commands {
+			fmt.Printf("%s: %s\n", c.name, c.description)
+		}
+		return nil
+	}
+
+	commands["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	}
+	commands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	}
 
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
-		fmt.Printf("Your command was: %v\n", cleanInput(scanner.Text())[0])
+		found := false
+		input := cleanInput(scanner.Text())
+		for _, c := range commands {
+			if c.name == input[0] {
+				c.callback()
+				found = true
+			}
+		}
+		if !found {
+			fmt.Printf("Unknown command: %s\n", input[0])
+		}
 	}
 }
 
